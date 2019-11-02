202319620aeb8e3e9344b1519131036f76a56e49
tariqulislamtuhin <tarequlislam105150@gmail.com>
2019-11-02 21:11:07 +0600

    DBContext

--- Models/DbContextUniversity.cs
+++ Models/DbContextUniversity.cs
@@ -8,9 +8,9 @@ namespace HtmHelperAnnotation.Models
 {
     public class DbContextUniversity : DbContext
     {
-        //public DbSet<Student> Students { get; set; }
-        //public DbSet<Department> Departments { get; set; }
-        //public DbSet<Course> Courses { get; set; }
-        //public DbSet<StudentCourse> StudentCourses { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<StudentCourse> StudentCourses { get; set; }
     }
 }
