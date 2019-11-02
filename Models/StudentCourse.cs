c6c825ec05b187ed2f095bb1dcd18bca47fbcccb
tariqulislamtuhin <tarequlislam105150@gmail.com>
2019-11-02 20:46:09 +0600

    Seted Annotation

--- Models/StudentCourse.cs
+++ Models/StudentCourse.cs
@@ -10,8 +10,11 @@ namespace HtmHelperAnnotation.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public int StudentId { get; set; }
+        [Required]
         public int CourseId { get; set; }
+        [Required]
         public DateTime EnrollDate { get; set; }
     }
 }
